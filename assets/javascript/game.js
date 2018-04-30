cc82fb87a00273cdbdcafc492e635a1827cddcbc
Greatest Dane <DaneGoodman85@gmail.com>
2018-04-29 17:16:49 -0700

    I think I'm pretty much finished with this game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,14 +17,52 @@ var spanLosses = document.getElementById("losses");
 var spanGuessesLeft = document.getElementById("guesses-left");
 var spanGuessesSoFar = document.getElementById("guesses-sofar");
 
-// Create the function for the key press event //
+// Computer generates a random letter and user chooses a letter//
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-document.onkeyup = function(event) {
-    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-    var userGuess = event.key;
-    lettersGuessed.push(userGuess);
-    
-    
+// display the number of current guesses left //
+spanGuessesLeft.textContent = guessesLeft;
+
+//Create a game-over function to reset all of your variables//
+function gameReset() {
+    guessesLeft = 9;
+    lettersGuessed = []
+    spanGuessesLeft.textContent = guessesLeft;
+    spanGuessesSoFar.textContent = lettersGuessed;
+    gameOver = false;
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 }
+
+
+// Create the function for the key press event //
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    // Push all letters user guesses into the array of letters guessed //
+    lettersGuessed.push(userGuess);
+    // display letters guessed //
+    spanGuessesSoFar.textContent = lettersGuessed;
+    // subtract and display a guess //
+    guessesLeft --;
+    spanGuessesLeft.textContent = guessesLeft;
+
+    // write logic for a win or loss //
+    if (userGuess.toLowerCase() === computerGuess) {
+        alert("YOU WIN! The letter was: " + computerGuess);
+        wins ++;
+        gameOver = true;
+        spanWins.textContent = wins;
+    }
+    else if (guessesLeft === 0) {
+        alert("YOU LOSE! The letter was: " + computerGuess);
+        losses ++;
+        gameOver = true;
+        spanLosses.textContent = losses;
+    }
+    // Reset game when it ends //
+    if (gameOver === true) {
+        gameReset();
+    }
+};
+
 
 
